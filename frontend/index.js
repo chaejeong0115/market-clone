fdfd8681c4df8db987a3b499400d4c576454d0d5
Lee Chaejeong <91517248+chaejeong0115@users.noreply.github.com>
2026-05-21 15:11:21 +0900

    refactor frontend structure

---
+++ frontend/index.js
@@ -0,0 +1,65 @@
+const calcTime = (timestamp) => {
+  // 세계시간 기준이라 한국 시간으로 계산
+  const curTime = new Date().getTime() - 9 * 60 * 60 * 1000;
+  const time = new Date(curTime - timestamp);
+  const hour = time.getHours();
+  const minite = time.getMinutes();
+  const second = time.getSeconds();
+
+  if (hour > 0) return ` ${hour}시간 전`;
+  else if (minite > 0) return ` ${minite}분 전`;
+  else if (second >= 0) return ` ${second}초 전`;
+  else " 방금 전";
+};
+
+const renderData = (data) => {
+  const main = document.querySelector("main");
+
+  data.reverse().forEach(async (obj) => {
+    const div = document.createElement("div");
+    div.className = "item-list";
+
+    const imgDiv = document.createElement("div");
+    imgDiv.className = "item-list__img";
+
+    const img = document.createElement("img");
+    const res = await fetch(`/images/${obj.id}`);
+    const blob = await res.blob();
+    const url = URL.createObjectURL(blob);
+    img.src = url;
+
+    const InfoDiv = document.createElement("div");
+    InfoDiv.className = "item-list__info";
+
+    const InfoTitleDiv = document.createElement("div");
+    InfoTitleDiv.className = "item-list__info-title";
+    InfoTitleDiv.innerText = obj.title;
+
+    const InfoMataDiv = document.createElement("div");
+    InfoMataDiv.className = "item-list__info-meta";
+    InfoMataDiv.innerText = `${obj.place} / ${calcTime(obj.insertAt)}`;
+
+    const InfoPriceDiv = document.createElement("div");
+    InfoPriceDiv.className = "item_list__info-price";
+    InfoPriceDiv.innerText = `${obj.price}원`;
+
+    imgDiv.appendChild(img);
+
+    InfoDiv.appendChild(InfoTitleDiv);
+    InfoDiv.appendChild(InfoMataDiv);
+    InfoDiv.appendChild(InfoPriceDiv);
+
+    div.appendChild(imgDiv);
+    div.appendChild(InfoDiv);
+
+    main.appendChild(div);
+  });
+};
+
+const fetchList = async () => {
+  const res = await fetch("/items");
+  const data = await res.json();
+  renderData(data);
+};
+
+fetchList();
